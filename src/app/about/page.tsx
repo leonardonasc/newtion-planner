4f211c2fec5e1775367d152d25316d886ad9a24b
Leonardo <leonardo.nasmt@gmail.com>
2026-07-26 00:20:02 -0300

    small feat: back button (commit for merge)

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,17 +1,24 @@
+import Link from "next/link";
 
 
 export default function AboutPage() {
     return (
         <div className="min-h-screen bg-white font-work-sans">
             <main className="mx-auto max-w-3xl px-6 py-16">
-                <header className="mb-12 border-b border-gray-200 pb-8">
-                    <span className="text-md font-bold uppercase tracking-wider text-blue-600">
-                        Sobre
-                    </span>
+                <header className="mb-12 border-b border-gray-200 pb-8 flex flex-col">
+                    <Link href="/" className="text-sm mb-7 font-medium text-blue-600 hover:underline">
+                        &larr; Voltar para a página inicial
+                    </Link>
 
-                    <h1 className="mt-2 text-4xl font-bold text-gray-900">
-                        Conheça um pouco mais sobre este projeto
-                    </h1>
+                    <div className="mt-4 flex flex-col">
+                        <span className="text-md font-bold uppercase tracking-wider text-blue-600">
+                            Sobre
+                        </span>
+
+                        <h1 className="text-4xl font-bold text-gray-900">
+                            Conheça um pouco mais sobre este projeto
+                        </h1>
+                    </div>
 
                     <p className="mt-4 text-lg leading-8 text-gray-500 font-normal">
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
