e128f87f6586839dfe0e23240e8f1850c609d1cb
Leonardo <leonardo.nasmt@gmail.com>
2026-07-27 22:34:56 -0300

    landing fix: layout was bugging

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -44,49 +44,37 @@ const ebGaramond = EB_Garamond({
   weight: ["400", "700"],
 });
 
-export const metadata: Metadata = {
-  title: "Newtion — Organize suas tarefas e gerencie seus gastos",
-  description: "Organize suas tarefas, acompanhe seus projetos e gerencie seus gastos de forma eficiente com o Newtion Planner. Nossa plataforma intuitiva e fácil de usar ajuda você a alcançar seus objetivos e manter o controle de suas atividades diárias.",
-};
-
 // public layout
 
-export default function RootLayout({
+export default function PublicLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
 
   return (
-    <html
-      lang="en"
-      className={cn("h-full", "antialiased", geistSans.variable, geistMono.variable, "font-mono", jetbrainsMono.variable, workSans.variable, "font-work-sans", arapey.variable, "font-arapey", caveat.variable, "font-caveat", ebGaramond.variable, "font-eb-garamond")}
-      suppressHydrationWarning
-    >
+    <>
+      <header>
+        <nav className="fixed top-0 left-0 right-0 z-50 p-2">
+          <LandingNavbar />
+        </nav>
+      </header>
+      <main>
+        {/* enablesystem desativado para manter apenas o whitetheme na aplicacao */}
+        <CustomCursor />
+        <SmoothScroll>
+          {children}
+        </SmoothScroll>
 
-      <body className="min-h-full flex flex-col bg-offwhite-50">
-        <header>
-          <nav className="fixed top-0 left-0 right-0 z-50 p-2">
-            <LandingNavbar />
-          </nav>
-        </header>
-        <main>
-          {/* enablesystem desativado para manter apenas o whitetheme na aplicacao */}
-          <CustomCursor />
-          <SmoothScroll>
-            {children}
-          </SmoothScroll>
+        <Toaster />
+        <TailwindIndicator />
+      </main>
 
-          <Toaster />
-          <TailwindIndicator />
-        </main>
-
-        <footer className="relative z-30 bg-offwhite-50 border-t border-gray-200">
-          <div className="max-w-6xl mx-auto p-4">
-            <Footer />
-          </div>
-        </footer>
-      </body>
-    </html>
+      <footer className="relative z-30 bg-offwhite-50 border-t border-gray-200">
+        <div className="max-w-6xl mx-auto p-4">
+          <Footer />
+        </div>
+      </footer>
+    </>
   );
 }
